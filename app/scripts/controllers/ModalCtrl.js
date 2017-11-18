7b0e759f8e39d3ef4b437a46bff6726d875e8a21
Jon Reck <jonathonreck@gmail.com>
2017-11-17 21:36:07 -0500

    modal functioning as planned

--- app/scripts/controllers/ModalCtrl.js
+++ app/scripts/controllers/ModalCtrl.js
@@ -1,15 +1,16 @@
 (function() {
-  function ModalCtrl(Room, $uibModalInstance) {
+  function ModalCtrl(Room, $uibModalInstance, room) {
     this.close = function () {
         $uibModalInstance.dismiss('cancel');
     };
 
-    this.add = Room.add;
-
+    this.add = function() {
+        Room.add(room);
+    };
   }
 
 
 angular
     .module('blocChat')
-    .controller('ModalCtrl', ['Room', '$uibModalInstance', ModalCtrl]);
+    .controller('ModalCtrl', ['Room', '$uibModalInstance', 'room', ModalCtrl]);
 })();
--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -3,14 +3,13 @@
        var Room = {};
        var ref = firebase.database().ref().child("rooms");
        var rooms = $firebaseArray(ref);
-
        Room.all = rooms;
 
        Room.add = function(room) {
            var name = document.getElementById('nameroom').value;
            console.log(name);
-           //room.name = name;
-           //console.log(room);
+           room.name = name;
+           console.log(room);
            rooms.$add(room);
        }
 
